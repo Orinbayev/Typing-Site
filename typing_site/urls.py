a4a8faff8acb11952b5d7ead74d44ece16967e09
Amirxon O'rinbayev <160326000+Orinbayev@users.noreply.github.com>
2025-08-15 16:46:51 +0500

    Update urls.py

--- typing_site/urls.py
+++ typing_site/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from typingapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("healthz", views.healthz, name="healthz"),
     path("", include(("typingapp.urls", "typingapp"), namespace="typingapp"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
